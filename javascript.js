203eb352bd2ffeed39f8326592397f66bd8a4c47
Melanie Hernandez <melan0617@gmail.com>
2023-02-08 12:58:48 -0800

    Working button and styling
    Added more styling to the page and button. Created a working button that hides/shows "Hello World!" when clicked.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
-var facts {
-    
+fact = ["Hello World!"]
+function generateFact() {
+    document.getElementById("randomFact").innerHTML = fact;
+}
+
+function hideShowFact() {
+    var x = document.getElementById("randomFact");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
 }
